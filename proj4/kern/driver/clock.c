8d9f2537147095b4696f262de85068f41dbd9872
= <=>
2016-04-08 10:17:53 +0800

    finish proj4

--- proj4/kern/driver/clock.c
+++ proj4/kern/driver/clock.c
@@ -19,7 +19,7 @@
 #define TIMER_FREQ			1193182
 #define TIMER_DIV(x)		((TIMER_FREQ + (x) / 2) / (x))
 
-#define TIMER_MODE			(IO_TIMER + 3)			//timer mode port
+#define TIMER_MODE			(IO_TIMER1 + 3)			//timer mode port
 #define TIMER_SEL0			0x00					//select counter 0
 #define TIMER_RATEGEN		0x04					//mode 2,rate generator
 #define TIMER_16BIT			0x30					//r/w counter 16 bits,LSB first
